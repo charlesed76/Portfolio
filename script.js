35df7ff0b7f1181d75f6440981f1f431a6cc40ba
charlesed76 <cokoumouna@gmail.com>
2024-12-18 15:47:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+
+	$(function() {
+		var header = $(".start-style");
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+		
+			if (scroll >= 10) {
+				header.removeClass('start-style').addClass("scroll-on");
+			} else {
+				header.removeClass("scroll-on").addClass('start-style');
+			}
+		});
+	});		
+		
+	//Animation
+	
+	$(document).ready(function() {
+		$('body.hero-anime').removeClass('hero-anime');
+	});
+
+	//Menu On Hover
+		
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});	
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("dark")) {
+			$("body").removeClass("dark");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("dark");
+			$("#switch").addClass("switched");
+		}
+	});  
+	
+  })(jQuery);
+    $(document).ready(function() {
+        $('#switch').on('click', function() {
+            // Toggle classes on the background section
+            $('.background-section').toggleClass('background-light background-dark');
+			$('.ptext').toggleClass('text-light text-dark');
+			$('h6.ptext').toggleClass('text-light text-dark');
+        });
+    });
+    $(document).ready(function() {
+        // Smooth scrolling to the portfolio section
+        $('a[href^="#"]').on('click', function(event) {
+            var target = $(this.getAttribute('href'));
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1000); // Durée de l'animation en millisecondes
+            }
+        });
+    });
+
+	
